dc334275805a6e5b2c155c79c10f32c96dd0a65b
frantuma <frantuma@yahoo.com>
2020-07-17 09:03:15 +0200

    initial prepare release PR

--- CI/prepare-release.sh
+++ CI/prepare-release.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-# get next release (from maven?, from last release? parseVersion..
-#SC_CUR_VERSION=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${projects.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
-#SC_CUR_VERSION=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${projects.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
-SC_VERSION=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}' --non-recursive build-helper:parse-version org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
-SC_NEXT_VERSION=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}' --non-recursive build-helper:parse-version org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
+export SC_VERSION=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}' --non-recursive build-helper:parse-version org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
+export SC_NEXT_VERSION=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}' --non-recursive build-helper:parse-version org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
 SC_QUALIFIER=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${parsedVersion.qualifier}' --non-recursive build-helper:parse-version org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
 SC_LAST_RELEASE=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${releasedVersion.version}' --non-recursive build-helper:released-version org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
-#export MY_POM_MAJOR_VERSION=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${parsedVersion.minorVersion}' --non-recursive build-helper:parse-version org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
-#export MY_POM_MAJOR_VERSION=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${parsedVersion.incrementalVersion}' --non-recursive build-helper:parse-version org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
-#export MY_POM_MAJOR_VERSION=`./mvnw -q -Dexec.executable="echo" -Dexec.args='${releasedVersion.version}' --non-recursive build-helper:released-version org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
-
 
 ##################################
-#####################
+##################################
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # !!!!!!!! removE !!!!!!!!!!!!!!!!!!!!!
 SC_LAST_RELEASE=2.1.3
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+##################################
+##################################
 
+
+echo "GHUSER $GH_USER"
 echo "version $SC_VERSION"
 echo "qual $SC_QUALIFIER"
 echo "last $SC_LAST_RELEASE"
@@ -26,37 +24,18 @@ CUR=$(pwd)
 SCRIPTDIR="$(dirname -- "${0}")/"
 BASEDIR="$SCRIPTDIR/../"
 
-echo "cur $CUR"
-echo "basedir $BASEDIR"
-echo "scriptdir $SCRIPTDIR"
-
 SC_RELEASE_TITLE="Swagger-core $SC_VERSION released!"
 SC_RELEASE_TAG="v$SC_VERSION"
-echo "title $SC_RELEASE_TITLE"
-echo "tag $SC_RELEASE_TAG"
 
 
-#when merged and if release run release action
-#		javadocs etc, update releasenotes, publish maven gradle, e finale metti next snapshot e fai PR e push PR direi
-# at this point master will run build, and possibly, as we are not in snapshot
-    # build and test the thing
-    # build and test gradle
-    # create javadocs and add to GH pages for annotations?
-    # publish maven central
-    # publish gradle
-    # publish prepared release notes
-    # update wiki
-    # update 1.5/master readme
-    # bump next snapshot
-
-
-# draft release Notes with next release after last release, with tag
-#$SCRIPTDIR/releaseNotes.py "$SC_LAST_RELEASE" "$SC_RELEASE_TITLE" "$SC_RELEASE_TAG"
+#####################
+### draft release Notes with next release after last release, with tag
+#####################
 $CUR/CI/releaseNotes.py "$SC_LAST_RELEASE" "$SC_RELEASE_TITLE" "$SC_RELEASE_TAG"
-#echo $SC_RELEASE_TITLE
-#echo $SC_RELEASE_NOTES
 
-# update the version to release in maven project with set version and backup using parseVersion maybe..
+#####################
+### update the version to release in maven project with set version
+#####################
 ./mvnw versions:set -DnewVersion=$SC_VERSION
 ./mvnw versions:commit
 
@@ -110,27 +89,15 @@ cd ./modules/swagger-gradle-plugin
 ./gradlew build --info
 cd ../..
 
+#####################
+### checkout branch prepare-release-2.x.x
+#####################
 
-#	checkout banch prepare-release-2.x.x
-#	add and commit to that branch
-#	push pR
+#####################
+### add and commit to that branch
+#####################
 
+#####################
+### push PR
+#####################
 
-#	e magari manually MERGE
-
-# add and commit
-# create PR
-# push PR
-
-#when merged and if release run release action
-#		javadocs etc, update releasenotes, publish maven gradle, e finale metti next snapshot e fai PR e push PR direi
-# at this point master will run build, and possibly, as we are not in snapshot
-    # build and test the thing
-    # build and test gradle
-    # create javadocs and add to GH pages for annotations?
-    # publish maven central
-    # publish gradle
-    # publish prepared release
-    # update wiki
-    # update 1.5/master readme
-    # bump next snapshot
