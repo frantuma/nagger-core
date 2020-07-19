78dfca238d141b58ab98931c71a3c59e3bb52d52
frantuma <frantuma@yahoo.com>
2020-07-19 10:07:03 +0200

    gpg and release action, staging skip

--- CI/post-release.sh
+++ CI/post-release.sh
@@ -27,21 +27,45 @@ BASEDIR="$SCRIPTDIR/../"
 
 SC_RELEASE_TAG="v$SC_VERSION"
 
-
 #####################
-### build and test maven ###
+### deploy gradle plugin release
 #####################
-./mvnw --no-transfer-progress -B install --file pom.xml
-
-#####################
-### build and test gradle ###
-#####################
-cd ./modules/swagger-gradle-plugin
+cd modules/swagger-gradle-plugin
+#./gradlew publishPlugins --info
 ./gradlew build --info
 cd ../..
+#####################
+### javadocs to ghpages ??? automatic???
+#####################
 
 #####################
-### deploy maven release TODO moved to acttion because of GPG issue
+### publish pre-prepared release (tag is created)
 #####################
-#./mvnw -Dgpg.passphrase=${GPG_PASSPHRASE} --no-transfer-progress -B deploy -Prelease --file pom.xml
-#./mvnw --no-transfer-progress -B install -Prelease --file pom.xml
+$CUR/CI/publishRelease.py "$SC_RELEASE_TAG"
+
+#####################
+### update the version to next snapshot in maven project with set version
+#####################
+./mvnw versions:set -DnewVersion=SC_NEXT_VERSION-SNAPSHOT
+./mvnw versions:commit
+
+#####################
+### update all other versions in files around to the next snapshot or new release, including readme and gradle ###
+#####################
+
+sc_find="version=$SC_VERSION"
+sc_replace="version=$SC_NEXT_VERSION-SNAPSHOT"
+sed -i -e "s/$sc_find/$sc_replace/g" $CUR/modules/swagger-gradle-plugin/gradle.properties
+
+sc_find="ft.nagger.core.v3:swagger-jaxrs2:$SC_VERSION"
+sc_replace="ft.nagger.core.v3:swagger-jaxrs2:$SC_NEXT_VERSION-SNAPSHOT"
+sed -i -e "s/$sc_find/$sc_replace/g" $CUR/modules/swagger-gradle-plugin/src/main/java/io/swagger/v3/plugins/gradle/SwaggerPlugin.java
+
+sc_find="name: 'swagger-jaxrs2', version:'$SC_VERSION"
+sc_replace="name: 'swagger-jaxrs2', version:'$SC_NEXT_VERSION-SNAPSHOT"
+sed -i -e "s/$sc_find/$sc_replace/g" $CUR/modules/swagger-gradle-plugin/src/test/java/io/swagger/v3/plugins/gradle/SwaggerResolveTest.java
+
+
+#####################
+### update wiki links
+#####################
