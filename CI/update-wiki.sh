6c7853c9b8dea7f9271b0882f26c45994c68b7b2
frantuma <frantuma@yahoo.com>
2020-07-21 13:35:51 +0200

    wiki fix path

--- CI/update-wiki.sh
+++ CI/update-wiki.sh
@@ -8,10 +8,12 @@ ls -la $CUR
 #####################
 ### update Wiki
 #####################
+cd wiki
 sc_find="$SC_LAST_RELEASE\/"
 sc_replace="$SC_VERSION\/"
 sed -i -e "s/$sc_find/$sc_replace/g" $CUR/Swagger-2.X---Annotations.md
 git add -A
 git commit -m "update javadocs links to ${SC_VERSION}"
 git push -u origin master
+cd ..
 
